3a0ddaa64d69a73ea98ada58adf105ae5dc68243
kaz-tokuda <kazuya.0221tennis@icloud.com>
2019-09-18 17:31:44 +0900

    すべて表示される

--- app/src/main/java/com/example/walk_coupon/MainActivity.java
+++ app/src/main/java/com/example/walk_coupon/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.walk_coupon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button startButton = (Button)findViewById(R.id.button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button couponButton = (Button)findViewById(R.id.button2);
+        couponButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,CouponActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
